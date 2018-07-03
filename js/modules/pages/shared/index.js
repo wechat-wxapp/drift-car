a99e94573b4bd4aab4db52a8aceea5093a7129ab
haohui.zhou <fhlie11@qq.com>
2018-07-04 03:18:16 +0800

    新增日志输出

--- js/modules/pages/shared/index.js
+++ js/modules/pages/shared/index.js
@@ -634,7 +634,7 @@ export default class Shared extends UTIL {
      * 泡妞神器
      * */
     qrPage() {
-        console.log(localStorage.getItem('accessToken'));
+        console.log('当前localStorage: ', localStorage.getItem('accessToken'));
         localStorage.removeItem('accessToken');
         currentPage = 'qrPage';
         this.showPage('qr', {}, true);
