c43dc09279113f6786dbcf8c36683af187f7d992
haohui.zhou <fhlie11@qq.com>
2018-07-04 01:40:46 +0800

    更新加速区间

--- js/bus.js
+++ js/bus.js
@@ -86,7 +86,7 @@ const basicData = {
     speedStepMax: 0.08,
     // 加速等级区间
     level: 0,
-    levelSpeed: [5, 20, 30, 40, 50, 60, 35, 40, 45, 50],
+    levelSpeed: [5, 20, 40, 60, 80, 90, 120, 160, 200, 250],
 
     timeStep: 1.0 / 60.0,
 
