7798502787f8c33dea6f6d6c7a7149ebfba21e08
haohui.zhou <fhlie11@qq.com>
2018-07-16 18:36:44 +0800

    更新分享图片

--- js/libs/wx.js
+++ js/libs/wx.js
@@ -11,7 +11,7 @@ export default class WX extends UTIL {
 
     shareObj = {
         title: '昨晚我输给一辆AE86，他用惯性漂移过弯，是你吗？',
-        imageUrl: 'https://static.cdn.24haowan.com/24haowan/test/js/share.png?v=1.0.0'
+        imageUrl: 'https://static.cdn.24haowan.com/24haowan/test/js/share.png?v=1.0.2'
     };
 
     isLogin = false;
@@ -77,6 +77,7 @@ export default class WX extends UTIL {
         this.startBtn.hide();
 
         this.startBtn.onTap((res) => {
+            console.log('res: ', res);
             if (this.tapLock) return false;
             this.tapLock = true;
             this.wxUnionId(res);
