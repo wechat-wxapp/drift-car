543c87df003748a668409371bb9dc0265756b916
haohui.zhou <fhlie11@qq.com>
2018-07-20 16:25:11 +0800

    删除加载模型旧方法

--- js/modules/util.js
+++ js/modules/util.js
@@ -3,51 +3,6 @@
  * */
 export default class UTIL {
     /**
-     * 创建模型
-     * @params model {String} 需要加载的模型地址
-     * @params material {String} 需要加载的纹理地址
-     * @params callback {Function} 加载完成后回调方法
-     */
-    createObj(model, material, callback) {
-        const manager = new THREE.LoadingManager();
-        manager.onProgress = (item, loaded, total) => {
-            // console.log(item, loaded, total);
-        };
-
-        const texture = new THREE.Texture();
-        texture.minFilter = THREE.LinearFilter;
-        const imgLoader = new THREE.ImageLoader(manager);
-        imgLoader.load(material, (image) => {
-            texture.image = image;
-            texture.needsUpdate = true;
-
-            // var texture = THREE.ImageUtils.loadTexture(material);
-
-            // 加载模型
-            const objLoader = new THREE.OBJLoader();
-            objLoader.load(model, (obj) => {
-                // console.log('加载模型: ', model);
-                // var materialObj = new THREE.MeshBasicMaterial({
-                //   vertexColors: THREE.FaceColors,
-                //   overdraw: 0.5
-                // });
-
-                obj.traverse((child) => {
-                    if (child instanceof THREE.Mesh) {
-                        child.material.map = texture;
-                    }
-                });
-
-                callback(obj);
-            }, (xhr) => {
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-            }, (err) => {
-                console.log('发生了错误: ', model, err);
-            });
-        });
-    }
-
-    /**
      * 获取最后未知的元素
      */
     getLastRoad() {
